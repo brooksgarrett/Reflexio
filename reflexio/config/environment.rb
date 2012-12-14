a13f0b871aaee4b382d8d4d1b282884bca5e86d1
Brooks Garrett <brooks@brooksgarrett.com>
2012-12-13 22:54:29 -0500

    Added the source IP statically by way of locale setting. I should redo that later.

--- reflexio/config/environment.rb
+++ reflexio/config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Force environment
+ENV['RAILS_ENV'] ||= 'production'
+
 # Initialize the rails application
 Reflexio::Application.initialize!
